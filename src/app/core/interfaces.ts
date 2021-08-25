1a46e713fc904c686424044612d4e3d1e9d66e17
mohamed.zilali <mohamed.zilali@cgi.com>
2021-08-25 13:26:29 -0400

    Add form and controls

---
+++ src/app/core/interfaces.ts
@@ -0,0 +1,30 @@
+export enum PaymentFrequencies {
+  AcceleratedWeekly = 'Accelerated Weekly',
+  Weekly = 'Weekly',
+  AcceleratedBiWeekly = 'Accelerated Bi-weekly',
+  BiWeekly = 'Bi-Weekly (every 2 weeks)',
+  SemiMonthly = 'Semi-monthly (24x per year)',
+  Monthly = 'Monthly (12x per year)'
+}
+
+export enum PaymentFrequency {
+  AcceleratedWeekly = 'AcceleratedWeekly',
+  Weekly = 'Weekly',
+  AcceleratedBiWeekly = 'AcceleratedBiWeekly',
+  BiWeekly = 'BiWeekly',
+  SemiMonthly = 'SemiMonthly',
+  Monthly = 'Monthly',
+}
+
+export enum PrePaymentFrequencies {
+  OneTime = 'One time',
+  EachYear = 'Each year',
+  SameAsRegularPayment = 'Same as regular payment',
+}
+
+export enum PrePaymentFrequency {
+  OneTime = 'OneTime',
+  EachYear = 'EachYear',
+  SameAsRegularPayment = 'SameAsRegularPayment',
+}
+
--- src/app/mortgage-calculator/mortgage-calculator.component.ts
+++ src/app/mortgage-calculator/mortgage-calculator.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {PaymentFrequencies, PaymentFrequency, PrePaymentFrequencies, PrePaymentFrequency} from "../core/interfaces";
 
 @Component({
   selector: 'app-mortgage-calculator',
@@ -9,12 +10,30 @@ import {FormGroup} from "@angular/forms";
 export class MortgageCalculatorComponent implements OnInit {
 
   mortgageCalculatorFormGroup: FormGroup;
+  amortizationPeriodYearsList = Array.from({length: 30}, (_, i) => i + 1);
+  amortizationPeriodMonthsList = Array.from({length: 11}, (_, i) => i + 1);
+  termYearsList = Array.from({length: 10}, (_, i) => i + 1);
+  PaymentFrequencies = PaymentFrequencies;
+  PrePaymentFrequencies = PrePaymentFrequencies;
 
   constructor() {
-    this.mortgageCalculatorFormGroup = new FormGroup({})
+    this.mortgageCalculatorFormGroup = new FormGroup({
+      mortgageAmount: new FormControl(100000.00, Validators.required),
+      interestRate: new FormControl(5, [Validators.required, Validators.min(0.0), Validators.max(100)]),
+      amortizationPeriodYears: new FormControl(25, Validators.required),
+      amortizationPeriodMonths: new FormControl(0, Validators.required),
+      paymentFrequency: new FormControl(PaymentFrequency.Monthly, Validators.required),
+      term: new FormControl(5, Validators.required),
+      prepaymentAmount: new FormControl(0, Validators.required),
+      prepaymentFrequency: new FormControl(PrePaymentFrequency.OneTime, Validators.required),
+      startWithPayment: new FormControl(1, Validators.required),
+    })
   }
 
   ngOnInit(): void {
   }
 
+  submit(): void {
+    console.log(this.mortgageCalculatorFormGroup.getRawValue());
+  }
 }
